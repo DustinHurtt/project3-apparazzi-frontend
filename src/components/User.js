bc70f5b6d35e6986b2cc16672bc7b462d10697e4
Dustin Hurtt <dustin_hurtt@outlook.com>
2022-05-31 16:55:51 -0400

    made profile pictures consistently circular

--- src/components/User.js
+++ src/components/User.js
@@ -11,11 +11,14 @@ const User = (props) => {
       <div className="userLanding">
         <div className="homeContainer">
           <h3>{props.user.username} </h3>
-          <img
+
+          <div className="profilePhoto" style={{backgroundImage: `url(${props.user.imageUrl})`}}></div>
+
+          {/* <img
             src={props.user.imageUrl}
             alt="profile pic"
             className="profilePhoto"
-          />
+          /> */}
           <h4>{props.user.name}</h4>
           <h5>Member since {memberSince}</h5>
           <p>{props.user.location}</p>
